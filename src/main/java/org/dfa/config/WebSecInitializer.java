dc514e45831cf5904b14ec9aca2a5511d1e7c9e9
Harry Coa <rodol_2000@hotmail.com>
2020-08-22 11:33:49 -0500

    Initial commit

---
+++ src/main/java/org/dfa/config/WebSecInitializer.java
@@ -0,0 +1,8 @@
+
+package org.dfa.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecInitializer extends AbstractSecurityWebApplicationInitializer {
+    
+}
